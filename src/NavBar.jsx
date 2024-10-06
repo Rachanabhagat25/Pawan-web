9711367cdc402f4936693efbb3424f1e38c84f9a
Rachanabhagat25 <bhagatrachana02@gmail.com>
2024-10-06 22:53:41 +0530

    pawan label

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,18 +1,20 @@
 // NavBar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./App.css"; 
+import "./App.css";
 
 const NavBar = () => {
     return (
         <nav className='navBar'>
-            <ul>
-                <li><Link to="/">Users</Link></li>
-                <li><Link to="/cars">Cars</Link></li>
-                <li><Link to="/dailypayments">Daily Payments</Link></li>
-                <li><Link to="/filterdailypayment">filter Daily Payments</Link></li>
+            <ul style={{ width: '100%', display: 'inline-block' }}>
+                <li style={{ float: 'left' }}><Link to="/">Users</Link></li>
+                <li style={{ float: 'left' }}><Link to="/cars">Cars</Link></li>
+                <li style={{ float: 'left' }}><Link to="/dailypayments">Daily Payments</Link></li>
+                <li style={{ float: 'left' }}><Link to="/filterdailypayment">Filter Daily Payments</Link></li>
+                <li style={{ float: 'right', fontWeight: 'bold', color: 'white' }}><Link>-- Pawan/Sameer Cabs -- </Link></li> {/* Align right */}
             </ul>
         </nav>
+
     );
 };
 
